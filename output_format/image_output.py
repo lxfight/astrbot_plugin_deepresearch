c4e86bbb4e60a60e1f08c56464770b5f4f74ab95
lxfight <1686540385@qq.com>
2025-06-06 20:53:00 +0800

    feat: 新增 ImageOutputFormatter 类及格式化图片输出的方法

--- output_format/image_output.py
+++ output_format/image_output.py
@@ -2,3 +2,18 @@
 
 # 这里需要获取到 AstrBot 的事件对象 AstrMessageEvent
 # 通过事件对象直接构造消息链实现消息的发送，并更新任务状态
+from ..core.task_manager import TaskStatus, Task
+from astrbot.api.event import AstrMessageEvent, MessageChain
+from astrbot.api import logger
+
+
+class ImageOutputFormatter:
+    @staticmethod
+    def format_image_output(event: AstrMessageEvent, task: Task):
+        """
+        格式化图片输出，并发送消息。
+
+        :param event: 触发任务的事件对象
+        :param task: 任务对象
+        """
+        pass
